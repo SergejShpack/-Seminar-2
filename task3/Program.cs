d70a115cdc0195b2c48e08104eb8b66ce1af3411
«Segej» <nikolaev.sergej2018@yandex.com>
2022-03-31 20:17:47 +0300

    t3

---
+++ task3/Program.cs
@@ -0,0 +1,12 @@
+﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+
+    
+    
+        Console.WriteLine("введите номер дня недели: ");
+       
+        int number = int.Parse(Console.ReadLine());
+
+           if (number == 6 || number == 7) Console.WriteLine("Выходной");
+       
+           else Console.WriteLine("Будний день");
+
