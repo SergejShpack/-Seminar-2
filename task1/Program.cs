e9995dd75687c639bc18d1948a6f2807fab2ae38
«Segej» <nikolaev.sergej2018@yandex.com>
2022-03-31 19:57:25 +0300

    t1

---
+++ task1/Program.cs
@@ -0,0 +1,10 @@
+﻿// Показать числа от -N до N
+
+Console.WriteLine("Введите число: ");
+       
+        int number = int.Parse(Console.ReadLine());
+       
+        int i = -number;
+
+        while (i <= number) Console.Write(i++ + " ");
+    
