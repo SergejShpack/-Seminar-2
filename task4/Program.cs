0b1667d07b796113fff116bb2871fcfd91f8eadc
«Segej» <nikolaev.sergej2018@yandex.com>
2022-03-31 20:28:57 +0300

    t4

---
+++ task4/Program.cs
@@ -0,0 +1,13 @@
+﻿// По двум заданным числам проверять является ли одно квадратом другого
+
+   
+    
+        Console.WriteLine("Введите 2 числа: ");
+        int num1 = int.Parse(Console.ReadLine());
+        
+        int num2 = int.Parse(Console.ReadLine());
+
+           if (num1 == num2 * num2) Console.WriteLine(num1 + " является " + num2 + " в квадрате");
+           else if (num2 == num1 * num1) Console.WriteLine(num2 + " является " + num1 + " в квадрате");
+           else Console.WriteLine("Не является");
+    
